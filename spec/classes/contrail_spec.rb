bf2172adf78eea0e6739543dfeb5dfa530954798
Harish Kumar <hkumarmk@gmail.com>
2014-09-17 04:56:16 +0000

    Added contrail ifmap-server management code
    Added contrail ifmap-server management code.

--- spec/classes/contrail_spec.rb
+++ spec/classes/contrail_spec.rb
@@ -9,9 +9,21 @@ describe 'contrail' do
     :lsbdistcodename => 'trusty'
     }
   end
+
   context 'with defaults' do
     it do
       should contain_class('contrail::system_config')
+      should contain_class('contrail::ifmap').with({
+        'control_ip_list' => ['10.1.1.1'],
+      })
     end
+  end
+
+  context 'with control node' do
+    let (:params) { { :control_ip_list => ['10.1.1.1','10.1.1.2','10.1.1.3'] } }
+
+    it {  should contain_class('contrail::ifmap').with({
+        'control_ip_list' => ['10.1.1.1','10.1.1.2','10.1.1.3'],
+      }) }
   end
 end
